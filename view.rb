480b3abaddfef7d4ffd295ae50642495961f70a1
Alex Fiore <writefiore@gmail.com>
2013-07-29 10:48:09 -0700

    add initial view file

---
+++ view.rb
@@ -0,0 +1,37 @@
+class View
+ 
+  class << self
+
+    def guess
+      @guess
+    end
+ 
+    def guess=(input)
+      @guess = input
+    end
+
+    def get_guess
+      print "Please enter a guess: "
+      guess = gets.chomp
+    end
+ 
+    def correct
+      puts "Correct"
+      puts "*"
+    end
+ 
+    def incorrect(card_def)
+      puts "Almost.... try again!"
+      puts 
+      definition(card_definition)
+    end
+
+    def definition(card_def)
+      puts "Definition"
+      puts "#{card_definition}"
+      puts
+    end
+
+  end
+
+end
